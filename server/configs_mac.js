f84770468ba85a77c1f41ba73e1ec50795b9e891
幼麟 <geniusalex@foxmail.com>
2017-09-14 14:56:06 +0800

    add start shell for mac.

--- server/configs_mac.js
+++ server/configs_mac.js
@@ -1,4 +1,4 @@
-﻿var HALL_IP = "127.0.0.1";
+﻿var HALL_IP = "127.0.0.1";//如果非本机访问，这里要变
 var HALL_CLIENT_PORT = 9001;
 var HALL_ROOM_PORT = 9002;
 
@@ -11,8 +11,8 @@ exports.mysql = function(){
 	return {
 		HOST:'127.0.0.1',
 		USER:'root',
-		PSWD:'123456',
-		DB:'db_scmj',
+		PSWD:'123456',//如果连接失败，请检查这里
+		DB:'db_scmj',//如果连接失败，请检查这里
 		PORT:3306,
 	}
 }
